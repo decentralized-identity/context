6ab7c579b50174ccc495b3d387b040678c5f0982
Orie Steele <orie@ohs.io>
2020-02-08 10:50:03 -0600

    chore: wip

--- tests/did-method-conformance.spec.js
+++ tests/did-method-conformance.spec.js
@@ -2,15 +2,21 @@ jest.setTimeout(20 * 1000);
 
 const useSecurityContext = require("../use-security-context");
 const documentLoader = require("./__fixtures__/documentLoader");
+const schema = require("../schemas");
 let canSignAndVerify;
 
 const urConfig = require("./__fixtures__/universal-resolver/config.json");
 
 const methodsForTest = {};
+
+const focusedMethods = ["key", "ethr"];
+
 urConfig.drivers.forEach(driver => {
   if (driver.image !== "universalresolver/driver-dns") {
     const methodName = driver.pattern.split(":")[1];
-    methodsForTest[methodName] = driver.testIdentifiers;
+    if (focusedMethods.indexOf(methodName) !== -1) {
+      methodsForTest[methodName] = driver.testIdentifiers;
+    }
   }
 });
 
@@ -73,6 +79,25 @@ const makeW3Test = methodsForTest => {
   });
 };
 
-makeDefaultTest(methodsForTest);
-makeW3Test(methodsForTest);
-makeDifTest(methodsForTest);
+const makeDifJsonSchemaTest = methodsForTest => {
+  describe("DIF JSON Schema", () => {
+    Object.keys(methodsForTest).map(method => {
+      describe(method, () => {
+        methodsForTest[method].forEach(did => {
+          it(did, async () => {
+            const { document } = await documentLoader(did, "default");
+            schema.validator.validate(document, schema.schemas.didDoc, {
+              throwError: true
+            });
+          });
+        });
+      });
+    });
+  });
+};
+
+makeDifJsonSchemaTest(methodsForTest);
+
+// makeDefaultTest(methodsForTest);
+// makeW3Test(methodsForTest);
+// makeDifTest(methodsForTest);
